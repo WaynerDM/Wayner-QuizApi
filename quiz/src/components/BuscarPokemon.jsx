474110f63cdd48d697922d4ac50b03e60706b4aa
Wayner Mora <morawayner3@gmail.com>
2026-04-18 11:29:44 -0600

    Router

---
+++ quiz/src/components/BuscarPokemon.jsx
@@ -0,0 +1,39 @@
+import { useState} from 'react'
+
+export default function BuscarPokemon() {
+    const [nombre, setNombre] = useState("")
+    const [pokemon, setPokemon] = useState(null);
+
+const buscarPokemon = async() => {
+    try {
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${nombre.toLowerCase()}`);
+        const data = await response.json();
+        setPokemon(data);
+    } catch (error) {
+        console.error('Error al buscar pokemon:', error);
+        setPokemon(null);
+    }
+};
+
+  return (
+    <div>
+      <h1>Buscar Pokemon</h1>
+      <input
+        type="text"
+        placeholder="Escribe un Pokemon"
+        value={nombre}
+        onChange={(e) => setNombre(e.target.value)}
+      />
+      <button onClick={buscarPokemon}>Buscar</button>
+
+      {pokemon && (
+        <div>
+          <h2>{pokemon.name}</h2>
+          <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+          <p>Altura: {pokemon.height}</p>
+            <p>Peso: {pokemon.weight}</p>
+        </div>
+      )}
+    </div>
+    );
+};
